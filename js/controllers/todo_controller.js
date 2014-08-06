ac85f5b91529d62c3d3482a60a96b548b13a8aa5
Ridhwana <Ridhwana.khan16@gmail.com>
2014-08-06 21:27:23 +0200

    feature: todo itemController, that implements the isCompleted property

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,29 @@
+// matches the name todo as an itemController in the template.
+Todos.TodoController = Ember.ObjectController.extend({
+
+// When called from the template to display the current isCompleted state of the todo,
+// this property will proxy that question to its underlying model.
+// When called with a value because a user has toggled the checkbox in the template, 
+// this property will set the isCompleted property of its model to the passed value (true or false), 
+// persist the model update, and return the passed value so the checkbox will display correctly.
+
+// The isCompleted function is marked a computed property whose value is 
+// dependent on the value of model.isCompleted.
+
+  isCompleted: function(key, value){
+    // the key is 'isCompleted', 
+    // the value can be either undefined or true if you checking a list item that already exists 
+    var model = this.get('model')
+
+    if(value === undefined){
+      // property being used as a getter
+      return model.get('isCompleted')
+    }
+    else{
+      // property being used as a setter
+      model.set('isCompleted', value),
+      model.save()
+      return value
+    }
+  }.property('model.isCompleted')
+});
