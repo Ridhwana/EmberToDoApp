ebfd6bdf64bc178d4ffd9661ed5e8f4275e714e4
Ridhwana <Ridhwana.khan16@gmail.com>
2014-08-09 19:43:24 +0200

    comments: add comments on how we setup using qunit and ember-qunit, referance: http://codingvalue.com/blog/emberjs-testing-setup/ and http://sssslide.com/speakerdeck.com/coderberry/the-unofficial-official-ember-testing-guide

--- js/tests/test.js
+++ js/tests/test.js
@@ -1,12 +1,21 @@
+// put Ember into testing mode, which will disable the automatic run loop behavior. 
+// This is needed because we do not want asynchronous functions firing in between or during other tests. We don't need to worry about this,
+// Ember is going to handle everything for us.
+
 App = Ember.Application.create({
   rootElement: '#qunit-fixture'
 });
 
+// turn on testing mode
 App.setupForTesting();
+
+// inject test helpers onto window
 emq.globalize();
 
+// if you don't have a custom resolver, setup one:
 App.Resolver = Ember.DefaultResolver.extend({namespace: App});
 setResolver(App.Resolver.create());
+
 
 test("should be an ember applicaiton", function() {
   equal(
