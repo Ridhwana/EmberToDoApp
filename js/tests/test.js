69770d397f804df377bc97ebcc759d4bb4fd57fd
Ridhwana <Ridhwana.khan16@gmail.com>
2014-08-09 23:42:14 +0200

    fix: no need to recreate an Ember application, we set a root element of the creted Ember app from the application.js

--- js/tests/test.js
+++ js/tests/test.js
@@ -2,10 +2,7 @@
 // This is needed because we do not want asynchronous functions firing in between or during other tests. We don't need to worry about this,
 // Ember is going to handle everything for us.
 
-App = Ember.Application.create({
-  rootElement: '#qunit-fixture'
-});
-
+Todos.rootElement = '#qunit-fixture'
 // turn on testing mode
 App.setupForTesting();
 
